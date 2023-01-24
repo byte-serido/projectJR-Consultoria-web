d7911d705f298a67260968b80e9a99a75eaa0648
JFmaia <jfmaia741@gmail.com>
2023-01-24 16:31:10 -0300

    feat: Criando rotas da aplicação #1

---
+++ web/src/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+// Registrando a Router no Vue
+Vue.use(Router)
+
+const router = new Router({
+    mode: 'history',
+    routes:[
+        
+    ]
+});
+
+export default router;
