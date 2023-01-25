f1a6ecd4c58f28115ba0b08214e4692e386c0f19
JFmaia <jfmaia741@gmail.com>
2023-01-24 22:50:49 -0300

    feat: Trocando name do componente #1

---
+++ web/src/components/home/MyHero.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <section class="hero-container">
+        <div class="container hero">
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    
+</style>
