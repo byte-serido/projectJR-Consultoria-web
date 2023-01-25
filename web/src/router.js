ad317b1a62e91260f6434bb1c811f49382bf4907
JFmaia <jfmaia741@gmail.com>
2023-01-25 12:55:14 -0300

    feat: Adicionando novas rotas #1

--- web/src/router.js
+++ web/src/router.js
@@ -1,12 +1,53 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from "./views/MyHome.vue"
+
+//Configuração pra renderização tardia, pois os imports só viram quando for realmente necessario.
+const Home = () => import('./views/MyHome.vue');
+const About = () => import('./views/MyAbout.vue');
+const Services = () => import('./views/MyServices.vue');
+const Blog = () => import('./views/MyBlog.vue');
+const Contact = () => import('./views/MyContact.vue');
 
 const routes = [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      //Permitindo que todos os parametros da rota sejam passados como atributos
+      props:true,
     },
+
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
+      //Permitindo que todos os parametros da rota sejam passados como atributos
+      props:true,
+    },
+
+    {
+      path: '/services',
+      name: 'services',
+      component: Services,
+      //Permitindo que todos os parametros da rota sejam passados como atributos
+      props:true,
+    },
+
+    {
+      path: '/blog',
+      name: 'blog',
+      component: Blog,
+      //Permitindo que todos os parametros da rota sejam passados como atributos
+      props:true,
+    },
+
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact,
+      //Permitindo que todos os parametros da rota sejam passados como atributos
+      props:true,
+    }
+
   ]
   
   const router = createRouter({
