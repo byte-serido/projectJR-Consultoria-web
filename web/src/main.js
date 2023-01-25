79d6abab4d81221e8f8e5be5fd9d93e466fd0bdb
JFmaia <jfmaia741@gmail.com>
2023-01-24 22:48:01 -0300

    feat: Adicionando as rotas. #1

--- web/src/main.js
+++ web/src/main.js
@@ -1,11 +1,5 @@
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import controller from "./controller/controller";
 
-new Vue({
-    //Registrando gerenciador de estado.
-    controller,
-    //Registrando as rotas no Vue
-    router:router,
-    render: h => h(App),
-  }).$mount('#app')
+createApp(App).use(router).mount('#app')
