406c7f65b984f4795e9952896dee5e5843c637d1
JFmaia <jfmaia741@gmail.com>
2023-01-24 22:49:09 -0300

    feat: Modificando arquivo router. #1

--- web/src/router.js
+++ web/src/router.js
@@ -1,14 +1,17 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from "./views/MyHome.vue"
 
-// Registrando a Router no Vue
-Vue.use(Router)
-
-const router = new Router({
-    mode: 'history',
-    routes:[
-        
-    ]
-});
-
-export default router;
+const routes = [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+  ]
+  
+  const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+  })
+  
+  export default router
