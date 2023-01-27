3c945e42e11f03f0903a6e73b672695f3611de30
Gabriel Lima <gabriel.lima2307@uol.com.br>
2023-01-27 19:43:22 -0300

    feat: Add import MyService.vue e Componente{Service}

--- web/src/views/MyHome.vue
+++ web/src/views/MyHome.vue
@@ -1,15 +1,17 @@
 <template scoped>
     <main>
         <MyHero id="hero"></MyHero>
+        <Service/>
     </main>
 </template>
 
 <script>
 
 import MyHero from "../components/home/MyHero.vue"
+import Service from "../components/home/MyService.vue"
 
 export default {
-    components:{MyHero},
+    components:{MyHero, Service},
 }
 </script>
 
