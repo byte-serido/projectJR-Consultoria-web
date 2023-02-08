6f00e4bdf096f03faba8c25b747ec9325db5de54
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-02-08 13:34:32 -0300

    chore: fix conflicts #15

--- web/src/views/MyHome.vue
+++ web/src/views/MyHome.vue
@@ -4,6 +4,7 @@
         <MyAbout></MyAbout>
         <MyService></MyService>
         <MyBlog></MyBlog>
+        <MyDepoiment></MyDepoiment>
     </main>
 </template>
 
@@ -12,9 +13,10 @@
 import MyHero from "../components/home/MyHero.vue"
 import MyAbout from "@/components/home/MyAbout.vue";
 import MyService from "../components/home/MyService.vue"
+import MyDepoiment from "@/components/home/MyDepoiment.vue";
 import MyBlog from "@/components/home/MyBlog.vue";
 export default {
-    components:{MyHero, MyAbout, MyService, MyBlog},
+    components:{MyHero, MyAbout, MyService, MyBlog, MyDepoiment},
 }
 </script>
 
