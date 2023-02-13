fb44420f6345a555f0c86f8dbab68605a55164b6
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-02-13 16:11:04 -0300

    feat: add initial responsive layout #15

--- web/src/components/home/MyBlog.vue
+++ web/src/components/home/MyBlog.vue
@@ -1,55 +1,54 @@
 <template>
-    <section class="teste-container">
-        <div class="container teste">
+    <section class="blog-container">
+        <div class="container blog">
             <article>
                 <h2>CONHEÇA O NOSSO BLOG!</h2>
                 <p>Criamos conteúdos com insights valiosos que podem ajudar você e sua empresa. Confira algumas das nossas publicações abaixo:</p>
             </article>
             <div class="wrapper">
                 <div class="principal">
-                    <div id="test">
-
+                    <div id="principal-img">
                     </div>
                     <div class="text">
                         <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        <a href="#">Leia mais</a>
+                        <a href="#">Leia mais ></a>
                     </div>
                 </div>
                 <div class="post-column">
                     <div class="post">
-                        <div class="placeholder">
+                        <div class="post-img">
 
                         </div>
                         <div class="content">
                             <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <a href="#">Leia mais</a>
+                            <a href="#">Leia mais ></a>
                         </div>
                     </div>
                     <div class="post">
-                        <div class="placeholder">
+                        <div class="post-img">
                             
                         </div>
                         <div class="content">
                             <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <a href="#">Leia mais</a>
+                            <a href="#">Leia mais ></a>
                         </div>
                     </div>
                     <div class="post">
-                        <div class="placeholder">
+                        <div class="post-img">
                             
                         </div>
                         <div class="content">
                             <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            <a href="#">Leia mais</a>
+                            <a href="#">Leia mais ></a>
                         </div>
                     </div>
                 </div>
             </div>
-            <a href="#" id="last">Veja todos os posts do nosso blog</a>
+            <a href="#" id="last">Veja todos os posts do nosso blog ></a>
         </div>
     </section>
 </template>
@@ -60,137 +59,280 @@
 </script>
 
 <style scoped>
-.teste-container{
-    background-color: #F7F9FC;
-    font-family: 'Inter',sans-serif;
-}
-.teste{
-    padding: 60px 87px;
-    display: flex;
-    flex-direction: column;
-    gap: 32px;
-}
+    .blog-container{
+        background-color: #F7F9FC;
+        font-family: 'Inter',sans-serif;
+    }
+    .blog{
+        padding: 60px 61px;
+        display: flex;
+        flex-direction: column;
+        gap: 32px;
+    }
 
-article{
-    color: #023F5C;
-}
+    article{
+        color: #023F5C;
+    }
 
-article h2{
-    font-weight: 700;
-    font-size: 2.5rem;
-    line-height: 140%;
-}
+    article h2{
+        font-weight: 700;
+        font-size: 2.5rem;
+        line-height: 140%;
+    }
 
-article p{
-    font-weight: 400;
-    font-size: 1.25rem;
-    line-height: 140%;
-}
+    article p{
+        font-weight: 400;
+        font-size: 1.25rem;
+        line-height: 140%;
+    }
 
-.wrapper{
-    display: flex;
-    gap: 22px;
-}
+    .wrapper{
+        display: flex;
+        gap: 22px;
+    }
 
-.principal{
-    max-width: 750px;
-    height: 560px;
-    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
-    border-radius: 10px;
-    background-color: #F7F9FC;
-}
+    .principal{
+        min-width: 50%;
+        height: 560px;
+        filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+        border-radius: 10px;
+        background-color: #F7F9FC;
+    }
 
-#test{
-    width: 600px;
-    height: 280px;
-    background-color: #1A688A;
-    border-top-left-radius: 10px;
-    border-top-right-radius: 10px;
-}   
+    #principal-img{
+        height: 300px;
+        background: url(../../assets/blog/desk-lamp-svgrepo-com.svg);
+        background-size: cover;
+    }   
 
-.text{
-    display: flex;
-    flex-direction: column;
-    width: 540px;
-    height: 230px;
-    padding: 30px 30px;
-    line-height: 140%;
-    gap: 15px;
-}
+    .text{
+        display: flex;
+        flex-direction: column;
+        height: 250px;
+        padding: 30px 30px;
+        line-height: 140%;
+        gap: 15px;
+    }
 
-.text h3{
-    color: #22749A;
-    font-weight: 700;
-    font-size: 1.75rem;
-    line-height: 140%;
-}
+    .text h3{
+        color: #22749A;
+        font-weight: 700;
+        font-size: 1.75rem;
+        line-height: 140%;
+    }
 
-.text p{
-    color: #4A5468;
-    font-size: 1.25rem;
-    line-height: 140%;
-    font-weight: 400;
-}
+    .text p{
+        color: #4A5468;
+        font-size: 1.25rem;
+        line-height: 140%;
+        font-weight: 400;
+    }
 
-.text a{
-    color: #1A688A;
-    font-size: 1.25rem;
-}
+    .text a{
+        color: #1A688A;
+        font-size: 1.25rem;
+    }
 
-.post-column{
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    max-width: 596px;
-    height: 560px;
-}
+    .post-column{
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        max-width: 570px;
+        height: 560px;
+    }
 
-.post{
-    display: flex;
-    align-items: center;
-    padding: 15px 16px;
-    gap: 32px;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 10px;
-}
+    .post{
+        display: flex;
+        align-items: center;
+        padding: 15px 16px;
+        gap: 32px;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 10px;
+    }
 
-.placeholder{
-    width: 450px;
-    height: 150px;
-    background-color: #1A688A;
-    border-radius: 10px;
-}
+    .post-img{
+        width: 500px;
+        height: 150px;
+        background-image: url(../../assets/blog/desk-lamp-svgrepo-com.svg);
+        background-color: #1A688A;
+        background-size: cover;
+        border-radius: 10px;
+    }
 
-.content{
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-}
+    .content{
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
 
-.content h3{
-    font-weight: 700;
-    font-size: 1.125rem;
-    line-height: 140%;
-    color: #22749A;
-}
+    .content h3{
+        font-weight: 700;
+        font-size: 1.125rem;
+        line-height: 140%;
+        color: #22749A;
+    }
 
-.content p{
-    font-size: 0.875rem;
-    line-height: 140%;
-    color: #4A5468;
-}
+    .content p{
+        font-size: 0.875rem;
+        line-height: 140%;
+        color: #4A5468;
+    }
 
-.content a{
-    font-weight: 400;
-    font-size: 0.75rem;
-    line-height: 140%;
-    color: #1A688A;
-}
+    .content a{
+        font-weight: 400;
+        font-size: 0.90rem;
+        line-height: 140%;
+        color: #1A688A;
+    }
 
-#last{
-    font-weight: 400;
-    font-size: 1.25rem;
-    line-height: 140%;
-    color: #1A688A;
-}
+    #last{
+        font-weight: 400;
+        font-size: 1.25rem;
+        line-height: 140%;
+        color: #1A688A;
+    }
+
+    @media (max-width:1098px){
+        .principal{
+            min-width: 40%;
+        }
+        .text{
+            min-width: 90%;
+            gap: 20px;
+        }
+        
+        .post-column{
+            max-width: 450px;
+        }
+        .content h3{
+            font-size: 1.10rem;
+            line-height: 100%
+        }
+        .content p{
+            line-height: 100%
+        }
+        #principal-img{
+            height: 230px;
+        }
+    }
+
+    @media(max-width:940px){
+        .text{
+            gap: 10px;
+        }
+
+        .text h3{
+            font-size: 1.50rem;
+        }
+        .post-column{
+            min-width: 300px;
+            display: flex;
+            justify-content: space-between;
+        }
+        .post-img{
+            height: 120px;
+        }
+        .content h3{
+            font-size: 1rem;
+            line-height: 100%
+        }
+        .content p{
+            font-size: 0.800rem;
+            line-height: 100%
+        }
+    }
+
+    @media(max-width:840px){
+        .post-column{
+            min-width: 250px;
+            gap: 5px;
+        }
+
+        .post{
+            padding: 10px 16px;
+            height: 150px;
+        }
+
+        .content{
+            gap: 5px;
+        }
+
+        .post-img{
+            height: 100px;
+        }
+
+        .text{
+            min-width: 80%;
+        }
+        .text h3{
+            font-size: 1.30rem;
+        }
+
+        .text p{
+            font-size: 1rem;
+        }
+        .text a{
+            font-size: 1rem;
+        }
+    }
+
+    @media (max-width: 640px){
+
+        .text{
+            gap: 20px;
+        }
+        .text h3{
+            font-size: 1.50rem;
+        }
+
+        .text p {
+            font-size: 1.25rem;
+        }
+
+        .text a {
+            font-size: 1.25rem;
+        }
+        .wrapper{
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+        }
+
+        .post-column{
+            max-width: 506px;
+        }
+    }
+
+    @media(max-width:560px){
+        article h2{
+            font-size: 1.9rem;
+        }
+        
+        article p{
+            font-size: 1.20rem;
+        }
+        .principal{
+            height: 460px;
+        }
+
+        #principal-img{
+            height: 150px;
+        }
+
+        .text{
+            gap: 15px;
+        }
+        .text h3{
+            font-size: 1.50rem;
+        }
+
+        #last{
+            font-size: 1.10rem;
+        }
+
+    }
+
+    @media(max-width: 460px){
+
+    }
 </style>
