de22bfddfe9ac961ed7d481df6e9802e62e9727f
Gabriel Lima <gabriel.lima2307@uol.com.br>
2023-02-25 11:24:03 -0300

    feat: criação de rota MyContact.vue

--- web/src/views/MyHome.vue
+++ web/src/views/MyHome.vue
@@ -16,14 +16,9 @@ import MyAbout from "@/components/home/MyAbout.vue";
 import MyService from "../components/home/MyService.vue"
 import MyDepoiment from "@/components/home/MyDepoiment.vue";
 import MyPostBlog from "@/components/home/MyPostBlog.vue";
-import MyContact from "@/components/home/MyContact.vue"
-export default {
-    components:{MyHero, MyAbout, MyService, MyDepoiment, MyContact},
-=========
-import MyPostBlog from "@/components/home/MyPostBlog.vue";
+import MyContact from "@/components/home/MyContact.vue";
 export default {
     components:{MyHero, MyAbout, MyService, MyPostBlog, MyDepoiment},
->>>>>>>>> Temporary merge branch 2
 }
 </script>
 
