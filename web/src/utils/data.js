699ba561ae4117f7b5912fe30ff4b4f82d9ed967
JFmaia <jfmaia741@gmail.com>
2023-02-28 09:41:40 -0300

    feat: Mock de dados false #25

---
+++ web/src/utils/data.js
@@ -0,0 +1,26 @@
+export default[
+    {
+        name: "José da Silva",
+        coment: "Trabalhei com a Project Jr, uma empresa com pessoas incriveis e atenciosas!",
+        office: "CEO",
+        company: "Alura"
+    },
+    {
+        name: "Maria Maia",
+        coment: "Trabalhei com a Project Jr é muito legal!",
+        office: "Gerente",
+        company: "DIO"
+    },
+    {
+        name: "Daniel Alves",
+        coment: "Trabalhei com a Project Jr, é realizar um sonho!",
+        office: "Funcionário",
+        company: "Nasa"
+    },
+    {
+        name: "Danilo Gorgonho",
+        coment: "Trabalhei com a Project Jr, é realizar um sonho! Maravilhosoooooo",
+        office: "Funcionário",
+        company: "Udemy"
+    }
+]
