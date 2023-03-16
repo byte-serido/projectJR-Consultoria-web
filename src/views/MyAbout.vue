40b9f54b0edce7d3cd6b5ff5914df2d86f54f2d5
Gabriel Lima <gabriel.lima2307@uol.com.br>
2023-03-16 14:52:51 -0300

    feat: implementação da rota (#22)

--- src/views/MyAbout.vue
+++ src/views/MyAbout.vue
@@ -1,14 +1,16 @@
 <template >
     <section>
+        <MyIntroAbout></MyIntroAbout>
         <MyCards></MyCards>
     </section>
 </template>
 
 <script>
+import MyIntroAbout from '@/components/about/MyIntroAbout.vue';
 import MyCards from '@/components/about/MyCards.vue';
 export default {
     components: {
-        MyCards
+        MyIntroAbout, MyCards
     }    
 }
 </script>
