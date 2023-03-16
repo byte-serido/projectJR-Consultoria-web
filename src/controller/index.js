88be1698190cb3c68f8779641bd2b8fbb2f32824
JFmaia <jfmaia741@gmail.com>
2023-03-16 18:16:46 -0300

    feat: Atualizando com novo modulo (#16)

--- src/controller/index.js
+++ src/controller/index.js
@@ -3,10 +3,11 @@ import { createStore } from 'vuex'
 //Modulos
 import homex from "@/controller/modules/home"
 import aboutx from "@/controller/modules/about"
-
+import genericx from "@/controller/modules/generic"
 export default createStore({
   modules: {
     homex,
-    aboutx
+    aboutx,
+    genericx
   }
 })
