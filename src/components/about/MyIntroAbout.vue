d9100b0062bcbad3d4a963900652e6c85df9cd9d
Gabriel Lima <gabriel.lima2307@uol.com.br>
2023-03-20 15:51:55 -0300

    feat: criação documentação MyIntroAbout.vue (#37)

--- src/components/about/MyIntroAbout.vue
+++ src/components/about/MyIntroAbout.vue
@@ -18,6 +18,11 @@ export default {
 }
 </script>
 <style scoped>
+
+    /* INICIO DO CSS */
+
+    /* ESTILIZAÇÃO DO CONTAINER INTOR ABOUT */
+
     .about-container{
         background-color: #023F5C;
     }
@@ -27,6 +32,8 @@ export default {
         display: flex ;
         justify-content: center;
     }
+
+    /* ESTILIZAÇÃO TITULO */
 
     article{
         padding: 142px 250px 142px 80px;
@@ -44,16 +51,6 @@ export default {
         
     }
 
-    span{
-        color: #4CB8E7;
-    }
-
-    img{
-        width: 300px;
-        height: 300px;
-        padding: 50px 171px 50px 0;
-    }
-
     p{
         font-family: 'Asap';
         font-style: normal;
@@ -62,25 +59,38 @@ export default {
         color: #F7F9FC;
         line-height: 160%;
     }
+
+    span{
+        color: #4CB8E7;
+    }
+
+    /* ESTILIZAÇÃO LOGO PROJECT.JR */
+
+    img{
+        width: 300px;
+        height: 300px;
+        padding: 50px 171px 50px 0;
+    }
+
+    /* RESPONSIVIDADE */
     
     @media(max-width:1130PX){
         article{
-        padding: 152px 150px 152px 80px;
+            padding: 152px 150px 152px 80px;
     }
     }
     
     @media(max-width:1060PX){
         article{
-        padding: 152px 100px 152px 80px;
-        justify-content: center;
-        text-align: center;
+            padding: 152px 100px 152px 80px;
+            justify-content: center;
+            text-align: center;
     }
 
+        img{
+            display: none;
+        }
 
-
-    img{
-        display: none;
-    }
     }
 
 
