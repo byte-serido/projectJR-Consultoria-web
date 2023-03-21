d8413dec5b3a720eb6f4db3e1f70d86978ebc562
Gabriel Lima <gabriel.lima2307@uol.com.br>
2023-03-21 12:03:53 -0300

    feat: documentação MyContact.vue (#37)

--- src/views/MyContact.vue
+++ src/views/MyContact.vue
@@ -4,7 +4,11 @@
     </section>
 </template>
 
+    <!-- O MENU CONTATOS VAI SER RESPONSAVÉL PARA ENTREGAR OS PRINCIPAIS MEIOS DE COMUNICAÇÃO E CONTATOS DA PROJECT JR. -->
+
 <script lang="js">
+
+    // AQUI VAI SER O MAPEAMENTO DO MENU CONTATOS
 export default {
     
 }
