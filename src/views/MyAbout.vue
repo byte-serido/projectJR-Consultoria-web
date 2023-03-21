037dd575e8b167ff7c2f898963d2de8ba5677290
Gabriel Lima <gabriel.lima2307@uol.com.br>
2023-03-21 12:03:01 -0300

    feat: documentação MyAbout.vue (#37)

--- src/views/MyAbout.vue
+++ src/views/MyAbout.vue
@@ -6,7 +6,12 @@
     </section>
 </template>
 
+    <!-- AQUI VAMOS ENTREGAR INFORMÇÃO SOBRE A PROJECT JR. COMO A DESCRIÇÃO DA PROJECT E A MISSÃO VISÃO E VALORES -->
+
 <script>
+
+    // AQUI FOI O MAPEAMENTO DO MENU ABOUT DO SITE
+
 import MyIntroAbout from '@/components/about/MyIntroAbout.vue'
 import MyDescription from '@/components/about/MyDescription.vue';
 import MyCards from '@/components/about/MyCards.vue';
