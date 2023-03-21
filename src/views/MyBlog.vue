7a07466df266619d2ac8522e66e10e4c85c7181a
Gabriel Lima <gabriel.lima2307@uol.com.br>
2023-03-21 12:03:36 -0300

    feat: documentação MyBlog.vue (#37)

--- src/views/MyBlog.vue
+++ src/views/MyBlog.vue
@@ -4,7 +4,12 @@
     </section>
 </template>
 
+    <!-- O MENU BLOG SERÁ RESPONSAVÉL POR DIVULGAR OS CONTEÚDOS DIVULGADOS PELA PROJECT JR. -->
+
 <script lang="js">
+
+    // AQUI VAI SER O MAPEAMENTO E IMPORTAÇÃO DOS COMPONENTES DO MENU BLOG
+
 export default {
     
 }
