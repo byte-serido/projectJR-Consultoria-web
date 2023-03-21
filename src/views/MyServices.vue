fb199b3cb77d486e17c4ed73aa8fa481d9be38c5
Gabriel Lima <gabriel.lima2307@uol.com.br>
2023-03-21 12:04:21 -0300

    feat: documentação MyService.vue (#37)

--- src/views/MyServices.vue
+++ src/views/MyServices.vue
@@ -4,7 +4,12 @@
     </section>
 </template>
 
+    <!-- O MENU SERVICE SERÁ RESPONSÁVEL PELA DOCUMENTAÇÃO DE TODOS OS SERVIÇOS PRESTADOS PELA PROJECT JR. -->
+
 <script>
+
+    // AQUI O MAPEAMENTO E IMPORTAÇÃO DOS COMPONENTS PARA MENU DE SERVIÇOS
+
 export default {
     
 }
