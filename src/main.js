908e798852f25273f6b51ad1a4f9a42519cc2bf0
JFmaia <jfmaia741@gmail.com>
2023-03-21 17:18:12 -0300

    feat: Adicionando o vuetify no app (#43)

--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ import App from './App.vue'
 import router from './router'
 import controller from './controller'
 
-createApp(App).use(controller).use(router).mount('#app')
+// Vuetify
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(vuetify).use(controller).use(router).mount('#app')
