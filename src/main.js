0021fa9706b42328e12bf03f48b9c22e93d20fba
JFmaia <jfmaia741@gmail.com>
2023-03-24 17:34:04 -0300

    feat: Adicionando o fontAwesome no projeto vue (#46)

--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import controller from './controller'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import specific icons */
+import {faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+/* add icons to the library */
+library.add(faEye,faEyeSlash)
 
-createApp(App).use(controller).use(router).mount('#app')
+
+createApp(App)
+    .component('fa', FontAwesomeIcon)
+    .use(controller)
+    .use(router)
+    .mount('#app');
