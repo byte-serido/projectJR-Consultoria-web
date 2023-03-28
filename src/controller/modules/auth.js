78cdf4eb64330726130d447386865e18b0e36862
JFmaia <jfmaia741@gmail.com>
2023-03-28 16:28:36 -0300

    feat: Create auth of login and lougot (#50)

--- src/controller/modules/auth.js
+++ src/controller/modules/auth.js
@@ -2,16 +2,29 @@ import axios from 'axios';
 import router from '@/router';
 export default{
     state:{
-        user:[]
+        user:{},
+        token:null
     },
     getters:{
         getUser(state){
             return state.user;
+        },
+        
+        isAuthenticated(state) {
+            return !!state.token
         }
     },
     mutations:{
         setLoginUser(state, user){
             state.user = user;
+        },
+
+        setToken(state, token) {
+            state.token = token
+        },
+
+        clearToken(state) {
+            state.token = null
         }
     },
     actions:{
@@ -21,12 +34,30 @@ export default{
                 password: user.password                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
               }).then(resp => {
                 if(resp.status === 201){
-                    commit("setLoginUser", resp.data);
+                    const token = resp.data.token;
+                    commit('setToken', token);
+                    localStorage.setItem('token', token);
+                    commit("setLoginUser", {username:resp.data.username, password:resp.data.password,mod:resp.data.mod});
                     router.push("/dashboard");
                 }
               }).catch(()=> {
                 return alert("Não foi encontrado nenhum login com tal username e senha fornecido! Por favor reveja seu username e senha!");
               });
-        }
+        },
+
+        logout({ commit }) {
+            commit('clearToken')
+            localStorage.removeItem('token')
+        },
+
+        async checkAuth({ commit, state }) {
+            const token = localStorage.getItem('token')
+            if (token && token !== state.token) {
+              commit('setToken', token)
+            }
+            if (!token && state.token) {
+              commit('clearToken')
+            }
+        },
     }
 }
