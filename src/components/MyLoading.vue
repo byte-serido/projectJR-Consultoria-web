5196290330891172e7b75e2fa6b033525bcc81f4
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-03-31 11:41:30 -0300

    feat(loading): add blur card (#54)
    increasing spinning card

--- src/components/MyLoading.vue
+++ src/components/MyLoading.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="loading">
-      <div class="spinner"></div>
+        <div class="card"></div>
+        <div class="spinner"></div>
     </div>
 </template>
-  
+
 <script>
 export default {
     
@@ -19,12 +20,28 @@ export default {
         height: 100%;
     }
 
+    .card{
+        height: 240px;
+        width: 246px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 40px;
+        gap: 20px;
+        background: #115A7B;
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        border-radius: 10px;
+        filter: blur(4px);
+        -webkit-filter: blur(4px);
+    }
+
     .spinner {
-        border: 3px solid #f3f3f3;
-        border-top: 3px solid #3498db;
+        position: absolute;
+        border: 4px solid #f3f3f3;
+        border-top: 4px solid #3498db;
         border-radius: 50%;
-        width: 30px;
-        height: 30px;
+        width: 80px;
+        height: 80px;
         animation: spin 1s linear infinite;
     }
 
@@ -33,4 +50,3 @@ export default {
         100% { transform: rotate(360deg); }
     }
 </style>
-  
