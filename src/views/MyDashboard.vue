a87f579e473f209b6ddbb6290c676480a869e8b2
JFmaia <jfmaia741@gmail.com>
2023-04-03 16:55:38 -0300

    feat: Struct of navbar success (#55)

--- src/views/MyDashboard.vue
+++ src/views/MyDashboard.vue
@@ -1,30 +1,34 @@
 <template>
+
     <!-- Nav bar superior -->
     <header>
+
         <!-- Coloca todos em linha -->
         <div class="navbar">
+
             <!-- // Div que contem tudo do usuario e o menu -->
             <div class="container-profile">
-                <div>
-                    <img src="" alt="">
-                </div>
+                <button>
+                    <img class="menu-icon" src="@/assets/dashboard/icon-list.svg" alt="Icone de menu de opções.">
+                </button>
                 <div class="avatar"></div>
                 <span>{{ username }}</span>
             </div>
 
             <!-- Button de saida -->
             <button @click="onLogout()">
-                <img src="" alt="">
+                <img class="button-icon" src="@/assets/dashboard/icon-log-out.svg" alt="Icone de indicado para sair.">
             </button>
         </div>
     </header>
     <main class="container dashbord">
+        <!-- Menu com animação se for ativado -->
         <transition>
             <template v-if="isMenu">
                 <div>oi</div>
             </template>
         </transition>
-        
+        <!-- Conteudo que ficara mudando dependendo da escolha no menu -->
         <div class="container-dashbord">
             <h1>ola</h1>
         </div>
@@ -77,14 +81,43 @@ export default {
         align-items: center;
         color: aliceblue;
         border-radius: 50%;
-        height: 40px;
-        width: 40px;
+        height: 38px;
+        width: 38px;
     }
 
     .container-profile span{
         color: #023F5C;
-        font-size: 1rem;
-        font-weight: 600;
+        font-size: 1.2rem;
+        font-weight: 700;
+    }
+
+    .container-profile button{
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        position: relative;
+        transition: all 400ms ease;
+        border-radius: 10%;
+    }
+
+    .container-profile button:active{
+        background-color: rgb(231, 226, 226);
+    }
+
+    .menu-icon{
+        height: 32px;
+        width: 32px;
+    }
+
+    .navbar button{
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+    }
+
+    .button-icon{
+        height: 26px;
+        width: 26px;
     }
 
     .dashbord{
