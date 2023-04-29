4ec2098b93b215be239cbc3b186d845c6a63b8bc
JFmaia <jfmaia741@gmail.com>
2023-04-29 18:50:29 -0300

    feat: Nova pasta de componetes (#75)

---
+++ src/components/dashboard/MyMembros.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Olá Membros</h1>
+    </div>
+</template>
+<script>
+export default{
+    
+}
+</script>
+<style scoped>
+    h1{
+        color: black;
+    }
+</style>
