30337a1b2c19fb7ab55247a6d232fcbecdad764c
JFmaia <jfmaia741@gmail.com>
2023-05-01 10:36:33 -0300

    feat: Importando novo icon (#75)

--- src/main.js
+++ src/main.js
@@ -8,9 +8,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
-import {faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import {faEye, faEyeSlash, faAdd } from '@fortawesome/free-solid-svg-icons'
 /* add icons to the library */
-library.add(faEye,faEyeSlash)
+library.add(faEye,faEyeSlash, faAdd)
 
 // Vuetify
 import { createVuetify } from 'vuetify'
