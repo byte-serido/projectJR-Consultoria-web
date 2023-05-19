93a35c6f391d64c6838652c57663c2b82c63c9a8
JFmaia <jfmaia741@gmail.com>
2023-05-19 10:24:39 -0300

    feat: Moving component (#75)

---
+++ src/components/MySpinnerLoading.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="loading">
+        <div class="spinner"></div>
+    </div>
+</template>
+
+<style scoped>
+    .loading {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+    }
+
+    .spinner {
+        position: absolute;
+        border: 4px solid #3498db;
+        border-top: 4px solid #f3f3f3;
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+        animation: spin 1s linear infinite;
+    }
+
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+</style>
