07da1cfa7262e5940816054e878f76ff2edda444
JFmaia <jfmaia741@gmail.com>
2023-05-19 11:20:56 -0300

    feat: Remove coment (#75)

--- src/controller/modules/membros.js
+++ src/controller/modules/membros.js
@@ -38,24 +38,5 @@ export default{
                 commit('setIsValid', true);
             })
         },
-        // Ao receber os dados sempre estavm undfined .
-        // async createMember({name,role,number,registration,description,imgUrl}){
-        //     await axios.post("https://pjr-api.onrender.com/member/create",{
-        //         name:name,
-        //         role:role,
-        //         number:number,
-        //         registration:registration,
-        //         description:description,
-        //         imgUrl:imgUrl
-        //     }).then(resp =>{
-        //         if(resp.status !== 200){
-        //             return console.log("Membro não cadastrado, verifique os dados e tente novamente !!")
-        //         }else{
-        //             return console.log("Membro cadastrado com sucesso!!")
-        //         }
-        //     }).catch((error)=>{
-        //         return console.log(error);
-        //     })
-        // }
     }
 }
