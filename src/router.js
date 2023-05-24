af69dd896599d4dc275aec6863603bb4a902ca3d
JFmaia <jfmaia741@gmail.com>
2023-05-23 22:24:10 -0300

    feat: Modified routes.

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ const Dashboard = () => import('./views/MyDashboard.vue');
 const Contact = () => import('./views/MyContact.vue');
   
   const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(process.env.BASE_URL),
     routes:[
       {
         path: '/',
