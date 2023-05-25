f5d6c9def5b16ade5bb3494b113a83a3832e3c0e
JFmaia <jfmaia741@gmail.com>
2023-05-25 16:48:09 -0300

    feat: ADD vuelidate in aplication #75

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import controller from './controller'
+import { VuelidatePlugin } from '@vuelidate/core';
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
+// Import do Quasar.
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
 /* import font awesome icon component */
@@ -18,5 +20,6 @@ createApp(App)
     .use(Quasar, quasarUserOptions)
     .component('fa', FontAwesomeIcon)
     .use(controller)
+    .use(VuelidatePlugin)
     .use(router)
     .mount('#app');
