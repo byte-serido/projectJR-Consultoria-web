992af1c14ea5633a01885135d43b1e1bc15b0d73
JFmaia <jfmaia741@gmail.com>
2023-05-24 22:47:24 -0300

    feat: Add new action, set and get #75

--- src/controller/modules/membros.js
+++ src/controller/modules/membros.js
@@ -3,15 +3,23 @@ import axios from 'axios';
 export default{
     state:{
         members:[],
+        member:{},
         valid:true,
+        validMember:true
     },
 
     getters:{
         getMembers(state){
             return state.members;
         },
+        getMember(state){
+            return state.member;
+        },
         getValid(state){
             return state.valid;
+        },
+        getValidMember(state){
+            return state.validMember;
         }
     },
 
@@ -19,8 +27,14 @@ export default{
         setMembers(state, member){
             state.members = member;
         },
+        setMember(state, member){
+            state.member = member;
+        },
         setValid(state, isValid){
             state.valid = isValid;
+        },
+        setValidMember(state, isValid){
+            state.validMember = isValid;
         }
     },
 
@@ -36,6 +50,19 @@ export default{
                 }
             }).catch(()=>{
                 commit('setValid', true);
+            })
+        },
+        async getMemberAction({commit},idMember){
+            await axios.get(`https://pjr-api.onrender.com/member/getone${idMember}`).then(resp =>{
+                if(resp.status === 200){
+                    commit('setMember',resp.data);
+                    commit('setValidMember', false);
+                    
+                }else{
+                    commit('setValidMember', true);
+                }
+            }).catch(()=>{
+                commit('setValidMember', true);
             })
         },
     }
