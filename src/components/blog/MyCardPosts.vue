73fb587d29d524a7adbdd4eae1cc0b508da1cd1c
israelsilva282 <israelcostas@hotmail.com>
2023-05-29 14:21:00 -0300

    style: fixing responsiviness (#87)

--- src/components/blog/MyCardPosts.vue
+++ src/components/blog/MyCardPosts.vue
@@ -21,6 +21,9 @@
       </div>
     </div>
   </div>
+  <div class="load-text">
+    <p>Carregar mais postagens</p>
+  </div>
 </template>
 <script>
 export default {
@@ -35,9 +38,10 @@ export default {
   justify-content: center;
 }
 .posts {
-  flex-wrap: wrap;
-  display: flex;
-  justify-content: space-between;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(415px, 1fr));
+  justify-items: center;
+  max-width: 90%;
   width: 1296px;
 }
 
@@ -79,6 +83,7 @@ export default {
   font-size: 28px;
   color: #22749a;
   text-decoration: none;
+  line-height: 140%;
 }
 
 .main-section p {
@@ -88,6 +93,7 @@ export default {
   font-size: 20px;
   line-height: 28px;
   color: #4a5468;
+  line-height: 140%;
 }
 
 .footer-section {
@@ -109,5 +115,113 @@ export default {
   font-weight: 400;
   font-size: 12px;
   color: #4cb8e7;
+}
+
+.load-text {
+  display: flex;
+  justify-content: center;
+  color: #0a4b69;
+  font-family: "Assistant";
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 171%;
+}
+
+@media (max-width: 1200px) {
+  .header-section {
+    font-size: 11px;
+  }
+
+  .main-section a {
+    font-size: 26px;
+  }
+
+  .main-section p {
+    font-size: 18px;
+    margin-bottom: 8px;
+  }
+  .profile p {
+    font-size: 16px;
+  }
+
+  .profile span {
+    font-size: 12px;
+  }
+
+  .footer-section img {
+    width: 51px;
+    height: 51px;
+  }
+}
+
+@media (max-width: 990px) {
+  .main-section a {
+    font-size: 24px;
+  }
+
+  .main-section p {
+    font-size: 16px;
+    margin-bottom: 8px;
+  }
+  .profile p {
+    font-size: 14px;
+  }
+
+  .profile span {
+    font-size: 12px;
+  }
+
+  .footer-section img {
+    width: 46px;
+    height: 46px;
+  }
+}
+
+@media (max-width: 768px) {
+  .card {
+    flex-direction: column;
+    height: 559px;
+    width: 415px;
+    max-width: 90%;
+  }
+  .img-post {
+    max-width: 100%;
+    background-repeat: no-repeat;
+    height: 224px;
+    background-size: cover;
+    border-radius: 10px 10px 0 0;
+  }
+
+  .post-section {
+    width: 415px;
+    height: 398px;
+  }
+
+  .footer-section {
+    margin-bottom: 22px;
+  }
+}
+
+@media (max-width: 600px) {
+  .main-section a {
+    font-size: 16px;
+  }
+
+  .main-section p {
+    font-size: 12px;
+    margin-bottom: 8px;
+  }
+  .profile p {
+    font-size: 12px;
+  }
+
+  .profile span {
+    font-size: 10px;
+  }
+
+  .footer-section img {
+    width: 42px;
+    height: 42px;
+  }
 }
 </style>
