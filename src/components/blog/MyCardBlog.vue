76526dfc7950d4ed4c2b38c21845e6dbda93eb94
israelsilva282 <israelcostas@hotmail.com>
2023-06-03 10:03:23 -0300

    fix: primeiras alterações (#87)

--- src/components/blog/MyCardBlog.vue
+++ src/components/blog/MyCardBlog.vue
@@ -1,33 +1,45 @@
 <template>
-  <div class="blog-container">
-    <div class="card">
-      <router-link :to="{ name: 'post', params: { id: post.id } }" :post="post">
-        <div class="img-post"></div>
-        <div class="post-section">
-          <div class="header-section">
-            <p>{{ post.category }}</p>
-            <span>{{ post.date }}</span>
-          </div>
-          <div class="main-section">
-            <h2>{{ post.title }}</h2>
-            <p>{{ post.description }}</p>
-          </div>
-          <div class="footer-section">
-            <img src="../../assets/blog/profile.svg" alt="Imagem de perfil" />
-            <div class="profile">
-              <p>{{ post.name }}</p>
-              <span>{{ post.role }}</span>
+  <div class="posts-container">
+    <div class="posts">
+      <div class="card" v-for="post in posts" :key="post.id">
+        <router-link :to="{ name: 'post', params: { id: post.id } }">
+          <div
+            class="img-post"
+            :style="{ background: `url(${post.img})` }"
+          ></div>
+          <div class="post-section">
+            <div class="header-section">
+              <p>{{ post.category }}</p>
+              <span>{{ post.date }}</span>
+            </div>
+            <div class="main-section">
+              <h2>{{ post.title }}</h2>
+              <p>{{ post.description }}</p>
+            </div>
+            <div class="footer-section">
+              <img src="../../assets/blog/profile.svg" alt="Imagem de perfil" />
+              <div class="profile">
+                <p>{{ post.name }}</p>
+                <span>{{ post.role }}</span>
+              </div>
             </div>
           </div>
-        </div>
-      </router-link>
+        </router-link>
+      </div>
     </div>
+  </div>
+  <div class="load-text">
+    <p>Carregar mais postagens</p>
   </div>
 </template>
 <script>
 export default {
   props: {
-    post: {},
+    posts: [],
+  },
+
+  mounted() {
+    this.posts.map((e) => console.log(e.img));
   },
 };
 </script>
@@ -36,47 +48,73 @@ a {
   text-decoration: none;
 }
 
+.card:nth-child(1) {
+  display: flex;
+  width: 81rem;
+  max-width: 100%;
+  height: 29.063rem;
+  margin-top: 2.875rem;
+  margin-bottom: 1.875rem;
+}
+
+.card:nth-child(1) > a {
+  flex-direction: row;
+}
+
+.card:nth-child(1) > a > .img-post {
+  width: 40.313rem;
+  height: 29.063rem;
+  max-width: 50%;
+}
+
+.card:nth-child(1) > a > .post-section {
+  width: 41.313rem;
+  max-width: 100%;
+}
+
 .card > a {
   display: flex;
   width: 100%;
+  height: 100%;
   justify-content: center;
+  flex-direction: column;
 }
-.blog-container {
+.posts-container {
   display: flex;
   justify-content: center;
-  align-items: center;
+}
+.posts {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  max-width: 90%;
+  width: 81rem;
 }
 
 .card {
-  display: flex;
-  width: 1296px;
-  max-width: 90%;
-  height: 465px;
-  margin-top: 46px;
-  margin-bottom: 30px;
+  width: 25.938rem;
+  background: #f7f9fc;
+  margin-bottom: 1.625rem;
 }
 
 .img-post {
-  width: 645px;
-  height: 465px;
-  max-width: 50%;
-  background: url("../../assets/blog/postImage.svg");
+  width: 25.938rem;
+  height: 14rem;
 }
 
 .post-section {
   display: flex;
+  flex: 1;
   flex-direction: column;
-  width: 661px;
-  max-width: 100%;
-  padding: 23px 8px 0 23px;
+  padding: 1.438rem 0.5rem 0 1.438rem;
   background: #f7f9fc;
 }
 
 .header-section {
   display: flex;
-  gap: 10px;
-  font-family: Inter;
-  font-size: 13px;
+  gap: 0.625rem;
+  font-family: "Inter";
+  font-size: 0.813rem;
   font-weight: 400;
   color: #4a5468;
 }
@@ -93,35 +131,31 @@ a {
 .main-section h2 {
   font-family: "Inter";
   font-weight: 700;
-  font-size: 28px;
+  font-size: 1.75rem;
   color: #22749a;
-  line-height: 140%;
   text-decoration: none;
+  line-height: 140%;
 }
 
 .main-section p {
+  flex: 1;
   font-family: "Inter";
   font-weight: 400;
-  font-size: 20px;
-  line-height: 140%;
+  font-size: 1.25rem;
   color: #4a5468;
+  line-height: 140%;
 }
 
 .footer-section {
   display: flex;
   align-items: center;
-  margin-bottom: 35px;
-}
-
-.profile {
-  display: flex;
-  flex-direction: column;
+  margin-bottom: 1.375rem;
 }
 
 .profile p {
   font-family: "Inter";
   font-weight: 600;
-  font-size: 18px;
+  font-size: 1.125rem;
   color: #023f5c;
   margin-bottom: 0;
 }
@@ -129,113 +163,166 @@ a {
 .profile span {
   font-family: "Asap";
   font-weight: 400;
-  font-size: 12px;
+  font-size: 0.75rem;
   color: #4cb8e7;
 }
 
-.footer-section img {
-  width: 51px;
-  height: 51px;
+.load-text {
+  display: flex;
+  justify-content: center;
+  color: #0a4b69;
+  font-family: "Assistant";
+  font-weight: 700;
+  font-size: 0.875rem;
+  line-height: 171%;
 }
 
 @media (max-width: 1200px) {
   .header-section {
-    font-size: 11px;
+    font-size: 0.688rem;
   }
 
   .main-section a {
-    font-size: 26px;
+    font-size: 1.625rem;
   }
 
   .main-section p {
-    font-size: 18px;
-    margin-bottom: 8px;
+    font-size: 1.125rem;
   }
   .profile p {
-    font-size: 16px;
+    font-size: 1rem;
   }
 
   .profile span {
-    font-size: 12px;
+    font-size: 0.75rem;
   }
 
   .footer-section img {
-    width: 51px;
-    height: 51px;
+    width: 3.188rem;
+    height: 3.188rem;
   }
 }
 
 @media (max-width: 990px) {
-  .main-section a {
-    font-size: 24px;
+  .card {
+    display: flex;
+    width: 81rem;
+    max-width: 100%;
+    height: 29.063rem;
+    margin-top: 2.875rem;
+    margin-bottom: 1.875rem;
+  }
+
+  .card > a {
+    flex-direction: row;
+  }
+
+  .img-post {
+    width: 40.313rem;
+    height: 29.063rem;
+    max-width: 50%;
+  }
+
+  .post-section {
+    width: 41.313rem;
+    max-width: 100%;
+  }
+
+  .main-section h2 {
+    font-size: 1.25rem;
   }
 
   .main-section p {
-    font-size: 16px;
-    margin-bottom: 8px;
+    font-size: 1rem;
   }
   .profile p {
-    font-size: 14px;
+    font-size: 0.875rem;
   }
 
   .profile span {
-    font-size: 12px;
+    font-size: 0.75rem;
   }
 
   .footer-section img {
-    width: 46px;
-    height: 46px;
+    width: 2.875rem;
+    height: 2.875rem;
   }
 }
 
 @media (max-width: 768px) {
-  .card {
-    height: 559px;
-    width: 415px;
-    max-width: 90%;
+  .main-section h2 {
+    font-size: 1.125rem;
   }
 
-  .card > a {
-    flex-direction: column;
+  .main-section p {
+    font-size: 0.875rem;
   }
-  .img-post {
-    max-width: 100%;
-    background-repeat: no-repeat;
-    height: 224px;
-    background-size: cover;
-    border-radius: 10px 10px 0 0;
+  .profile p {
+    font-size: 0.75rem;
   }
 
-  .post-section {
-    width: 415px;
-    height: 398px;
-  }
-
-  .footer-section {
-    margin-bottom: 22px;
+  .profile span {
+    font-size: 0.625rem;
   }
 }
 
 @media (max-width: 600px) {
-  .main-section a {
-    font-size: 16px;
+  .card:nth-child(n) {
+    height: 34.938rem;
+    width: 25.938rem;
+    max-width: 90%;
+  }
+
+  .card:nth-child(n) > a {
+    flex-direction: column;
+  }
+  .card:nth-child(n) > a > .img-post {
+    max-width: 100%;
+    background-repeat: no-repeat;
+    height: 14rem;
+    background-size: cover;
+    border-radius: 0.625rem 0.625rem 0 0;
+  }
+
+  .card:nth-child(n) > a > .post-section {
+    width: 25.938rem;
+  }
+
+  .posts {
+    justify-content: center;
+  }
+
+  .header-section {
+    font-size: 0.5rem;
+  }
+
+  .main-section h2 {
+    font-size: 0.875rem;
   }
 
   .main-section p {
-    font-size: 12px;
-    margin-bottom: 8px;
+    font-size: 0.625rem;
   }
   .profile p {
-    font-size: 12px;
+    font-size: 0.75rem;
   }
 
   .profile span {
-    font-size: 10px;
+    font-size: 0.625rem;
   }
 
   .footer-section img {
-    width: 42px;
-    height: 42px;
+    width: 2.625rem;
+    height: 2.625rem;
+  }
+}
+
+@media (max-width: 280px) {
+  .card:nth-child(n) > a > .img-post {
+    display: none;
+  }
+  .main-section h2 {
+    font-size: 0.75rem;
   }
 }
 </style>
--- src/views/MyBlog.vue
+++ src/views/MyBlog.vue
@@ -3,7 +3,7 @@
     <NavBar></NavBar>
   </header>
   <main>
-    <CardBlog :post="mainPost"></CardBlog>
+    <!-- <CardBlog :post="mainPost"></CardBlog> -->
     <CardPosts :posts="posts"></CardPosts>
     <img src="" alt="" />
   </main>
@@ -15,25 +15,24 @@
 <script>
 import Footer from "@/components/MyFooter.vue";
 import NavBar from "@/components/NavBar.vue";
-import CardBlog from "@/components/blog/MyCardBlog.vue";
 import CardPosts from "@/components/blog/MyCardPosts.vue";
 export default {
-  components: { NavBar, Footer, CardBlog, CardPosts },
+  components: { NavBar, Footer, CardPosts },
   data() {
     return {
-      mainPost: {
-        id: 1,
-        category: "Categoria",
-        date: "27 de jan, 2023",
-        title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rutrum ante a elit tristique, in pulvinar ligula interdum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla facilisi. Nulla facilisis risus id leo feugiat, a vestibulum urna tristique. Nam tristique, urna vitae lacinia semper, ipsum metus consequat dui, sed fermentum enim libero sed dui. Mauris viverra justo et turpis tincidunt, eget convallis justo tincidunt.",
-        name: "Fulano da silva",
-        profileImg: "",
-        role: "Cargo na empresa",
-      },
-
       posts: [
+        {
+          id: 1,
+          category: "Categoria",
+          date: "27 de jan, 2023",
+          title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce rutrum ante a elit tristique, in pulvinar ligula interdum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla facilisi. Nulla facilisis risus id leo feugiat, a vestibulum urna tristique. Nam tristique, urna vitae lacinia semper, ipsum metus consequat dui, sed fermentum enim libero sed dui. Mauris viverra justo et turpis tincidunt, eget convallis justo tincidunt.",
+          name: "Fulano da silva",
+          profileImg: "",
+          role: "Cargo na empresa",
+          img: "http://localhost:8080/img/postImage.d7e6d514.svg",
+        },
         {
           id: 2,
           category: "Categoria",
