ea19ef2141fe694ba5e7c76087d3910fdb916778
israelsilva282 <israelcostas@hotmail.com>
2023-06-22 08:19:56 -0300

    feat: add structure for post home page (#94)

--- src/components/dashboard/MyPostes.vue
+++ src/components/dashboard/MyPostes.vue
@@ -1,13 +1,185 @@
 <template>
-    <div>
-        <h1>Meus depoimentos!</h1>
+  <Loading v-if="isLoading"></Loading>
+  <div class="container-posts" v-else>
+    <div class="title-row">
+      <span>Posts</span>
+      <button @click="onRedirectCreateMember()">
+        <p>Novo Post</p>
+        <fa :icon="['fa', 'add']" style="color: #ffffff" size="lg" />
+      </button>
     </div>
+
+    <div class="grid-posts">
+      <Card></Card>
+    </div>
+  </div>
 </template>
 <script>
+import Loading from "@/components/MySpinnerLoading.vue";
+import Card from "./components/posts/CardPost.vue";
+// import Loading from "@/components/dashboard/MySpinnerLoading.vue"
 export default {
-    
-}
+  components: { Loading, Card },
+  methods: {
+    onRedirectCreateMember() {
+      return this.$router.push({ name: "create-member" });
+    },
+  },
+  computed: {
+    posts() {
+      return this.$store.getters.getPosts;
+    },
+
+    isLoading() {
+      return this.$store.getters.getValidPosts;
+    },
+  },
+  async mounted() {
+    await this.$store.dispatch("getPosts");
+  },
+};
 </script>
 <style scoped>
-    
+p {
+  margin: 0;
+  padding: 0;
+}
+.container-posts {
+  color: black;
+  display: flex;
+  flex-direction: column;
+  font-family: "Inter", sans-serif;
+  gap: 60px;
+  height: 100%;
+}
+
+.title-row {
+  display: flex;
+  gap: 40px;
+  align-items: center;
+}
+
+.title-row span {
+  color: #023f5c;
+  font-size: 1.4rem;
+  font-weight: 700;
+}
+
+.title-row button {
+  font-family: "Inter", sans-serif;
+  padding: 10px;
+  background-color: #023f5c;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  border-radius: 4px;
+  border: none;
+  color: #ffffff;
+  transition: all 400ms ease;
+}
+
+.title-row button:hover {
+  background-color: #013249;
+}
+
+.title-row button p {
+  font-size: 1rem;
+  font-weight: 300;
+}
+
+.grid-posts {
+  display: flex;
+  flex-wrap: wrap;
+  padding-bottom: 40px;
+  gap: 20px;
+}
+
+/* Responsive */
+@media (max-width: 860px) {
+  .container-posts {
+    gap: 40px;
+  }
+
+  .title-row {
+    gap: 30px;
+  }
+  .title-row span {
+    font-size: 1.2rem;
+  }
+  .title-row button {
+    padding: 6px;
+    gap: 16px;
+  }
+
+  .title-row button p {
+    font-size: 1rem;
+    font-weight: 300;
+  }
+
+  .grid-posts {
+    display: flex;
+    flex-wrap: wrap;
+    padding-bottom: 30px;
+    gap: 16px;
+  }
+}
+
+@media (max-width: 640px) {
+  .container-posts {
+    gap: 36px;
+  }
+
+  .title-row {
+    gap: 28px;
+  }
+  .title-row span {
+    font-size: 1.1rem;
+  }
+  .title-row button {
+    padding: 6px;
+    gap: 16px;
+  }
+
+  .title-row button p {
+    font-size: 1rem;
+    font-weight: 300;
+  }
+
+  .grid-posts {
+    display: flex;
+    flex-wrap: wrap;
+    padding-bottom: 28px;
+    gap: 16px;
+  }
+}
+
+@media (max-width: 440px) {
+  .container-posts {
+    gap: 30px;
+  }
+
+  .title-row {
+    gap: 26px;
+  }
+  .title-row span {
+    font-size: 1rem;
+  }
+  .title-row button {
+    padding: 6px;
+    gap: 16px;
+  }
+
+  .title-row button p {
+    font-size: 1rem;
+    font-weight: 300;
+  }
+
+  .grid-posts {
+    display: flex;
+    flex-wrap: wrap;
+    padding-bottom: 26px;
+    gap: 16px;
+  }
+}
 </style>
--- src/views/MyDashboard.vue
+++ src/views/MyDashboard.vue
@@ -1,290 +1,310 @@
 <template>
-    <!-- Nav bar superior -->
-    <header>
-        <!-- Coloca todos em linha -->
-        <div class="navbar">
+  <!-- Nav bar superior -->
+  <header>
+    <!-- Coloca todos em linha -->
+    <div class="navbar">
+      <!-- // Div que contem tudo do usuario e o menu -->
+      <div class="container-profile">
+        <button @click="isMenu = !isMenu">
+          <fa :icon="['fa', 'bars']" style="color: #ffffff" size="lg" />
+        </button>
+        <div class="avatar"></div>
+      </div>
+      <router-link to="/">
+        <img
+          class="logo-home"
+          src="@/assets/nav/logo-bar.svg"
+          alt="Imagem de Logo"
+        />
+      </router-link>
 
-            <!-- // Div que contem tudo do usuario e o menu -->
-            <div class="container-profile">
-                <button @click="isMenu=!isMenu">
-                    <fa :icon="['fa', 'bars']" style=" color: #FFFFFF;" size="lg"/>
-                </button>
-                <div class="avatar"></div>
-            </div>
-            <router-link to="/">
-                <img class="logo-home" src="@/assets/nav/logo-bar.svg" alt="Imagem de Logo">
-            </router-link>
-
-            <!-- Button de saida -->
-            <button @click="onLogout()">
-                <fa :icon="['fa', 'right-from-bracket']" style=" color: #FFFFFF;" size="lg"/>
-            </button>
-        </div>
-    </header>
-    <main class="dashbord">
-        <!-- Menu com animação se for ativado -->
-        <transition name="slide" mode="out-in">
-            <!-- Menu que irá aparecer quando for clicado -->
-            <div class="menu" v-if="isMenu">
-                <router-link class="item" to="" active-class="active" exact>
-                    <span>Administradores</span>
-                </router-link>
-                <router-link @click="isMenu=!isMenu" class="item" to="/membros" active-class="active">
-                    <span>Membros</span>
-                </router-link>
-                <router-link class="item" to="" active-class="active">
-                    <span>Serviços</span>
-                </router-link>
-                <router-link class="item" to="" active-class="active">
-                    <span>Depoimentos</span>
-                </router-link>
-                <router-link class="item" to="" active-class="active">
-                    <span>Posts</span>
-                </router-link>
-            </div>
-        </transition>
-        <!-- Conteudo que ficara mudando dependendo da escolha no menu -->
-        <div class="dashboard-content">
-            <router-view/>
-        </div>
-    </main>
+      <!-- Button de saida -->
+      <button @click="onLogout()">
+        <fa
+          :icon="['fa', 'right-from-bracket']"
+          style="color: #ffffff"
+          size="lg"
+        />
+      </button>
+    </div>
+  </header>
+  <main class="dashbord">
+    <!-- Menu com animação se for ativado -->
+    <transition name="slide" mode="out-in">
+      <!-- Menu que irá aparecer quando for clicado -->
+      <div class="menu" v-if="isMenu">
+        <router-link class="item" to="" active-class="active" exact>
+          <span>Administradores</span>
+        </router-link>
+        <router-link
+          @click="isMenu = !isMenu"
+          class="item"
+          to="/membros"
+          active-class="active"
+        >
+          <span>Membros</span>
+        </router-link>
+        <router-link class="item" to="" active-class="active">
+          <span>Serviços</span>
+        </router-link>
+        <router-link class="item" to="" active-class="active">
+          <span>Depoimentos</span>
+        </router-link>
+        <router-link class="item" to="/posts" active-class="active">
+          <span>Posts</span>
+        </router-link>
+      </div>
+    </transition>
+    <!-- Conteudo que ficara mudando dependendo da escolha no menu -->
+    <div class="dashboard-content">
+      <router-view />
+    </div>
+  </main>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            isMenu: false,
-            username:"",
-        }
+  data() {
+    return {
+      isMenu: false,
+      username: "",
+    };
+  },
+  methods: {
+    onLogout() {
+      this.$store.dispatch("logout");
+      // altera o estado
+      this.$store.dispatch("logout");
+      // adiciona o sufixo /login a URL da rota
+      this.$router.push("/login");
     },
-    methods:{
-        onLogout(){
-            this.$store.dispatch("logout");
-            // altera o estado 
-            this.$store.dispatch("logout")
-            // adiciona o sufixo /login a URL da rota
-            this.$router.push("/login");
-        }
-    },
-    created(){
-        this.username = window.localStorage.getItem('username');
-    }
-}
+  },
+  created() {
+    this.username = window.localStorage.getItem("username");
+  },
+};
 </script>
 <style scoped>
-    /* Nav Bar */
-    header{
-        font-family: 'Assistant', sans-serif;
-    }
-    .navbar{
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 16px 60px;
-        background-color: #023F5C;
-        border-bottom: 1px solid rgb(230, 227, 227);
-    }
+/* Nav Bar */
+header {
+  font-family: "Assistant", sans-serif;
+}
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px 60px;
+  background-color: #023f5c;
+  border-bottom: 1px solid rgb(230, 227, 227);
+}
 
-    .navbar .container-profile{
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        gap: 10px;
-    }
+.navbar .container-profile {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
 
-    .container-profile .avatar{
-        background-color: #FFFFFF;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 50%;
-        height: 30px;
-        width: 30px;
-    }
+.container-profile .avatar {
+  background-color: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  height: 30px;
+  width: 30px;
+}
 
-    .container-profile span{
-        color: #FFFFFF;
-        font-size: 1.2rem;
-        font-weight: 700;
-    }
+.container-profile span {
+  color: #ffffff;
+  font-size: 1.2rem;
+  font-weight: 700;
+}
 
-    .container-profile button{
-        background-color: transparent;
-        border: none;
-        cursor: pointer;
-        position: relative;
-        transition: all 400ms ease;
-        border-radius: 4px;
-    }
+.container-profile button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  position: relative;
+  transition: all 400ms ease;
+  border-radius: 4px;
+}
 
-    .container-profile button:active{
-        background-color: rgb(231, 226, 226);
-    }
+.container-profile button:active {
+  background-color: rgb(231, 226, 226);
+}
 
-    .navbar button{
-        background-color: transparent;
-        border: none;
-        cursor: pointer;
-    }
+.navbar button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
 
-    .logo-home{
-        height: 32px;
-        width: 32px;
-    }
+.logo-home {
+  height: 32px;
+  width: 32px;
+}
 
-    /* Dashboard */
-    .dashbord{
-        min-height: 100%;
-        width: 100%;
-        background-color:#e9eef2;
-        display: flex;
-        flex-direction: row;
-    }
-    /* Menu */
-    .menu{
-        background-color: #023F5C;
-        width: 260px;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        border-right: 1px solid rgb(230, 227, 227);
-        min-height: 100%;   
-    }
+/* Dashboard */
+.dashbord {
+  min-height: 100%;
+  width: 100%;
+  background-color: #e9eef2;
+  display: flex;
+  flex-direction: row;
+}
+/* Menu */
+.menu {
+  background-color: #023f5c;
+  width: 260px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  border-right: 1px solid rgb(230, 227, 227);
+  min-height: 100%;
+}
 
-    .menu .item{
-        padding: 22px;
-        box-sizing: border-box;
-        text-decoration: none;
-        display: flex;
-        width: 100%;
-        cursor: pointer;
-        position: relative;
-        transition: all 400ms ease;
-        border-bottom: 1px solid #e9eef2;
-    }
+.menu .item {
+  padding: 22px;
+  box-sizing: border-box;
+  text-decoration: none;
+  display: flex;
+  width: 100%;
+  cursor: pointer;
+  position: relative;
+  transition: all 400ms ease;
+  border-bottom: 1px solid #e9eef2;
+}
 
-    .menu .item:hover{
-        background-color: #042839;
-    }
+.menu .item:hover {
+  background-color: #042839;
+}
 
-    .menu .item span{
-        color: #FFFFFF;
-        font-size: 0.9rem;
-        font-weight: 500;
-    }
+.menu .item span {
+  color: #ffffff;
+  font-size: 0.9rem;
+  font-weight: 500;
+}
 
-    .menu .item img{
-        height: 18px;
-        width: 18px;
-    }
+.menu .item img {
+  height: 18px;
+  width: 18px;
+}
 
-    /* Body */
-    .dashboard-content {
-        padding: 60px 60px;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: space-between;
-        min-height: 100%;
-        width: 100%;
-    }
+/* Body */
+.dashboard-content {
+  padding: 60px 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  min-height: 100%;
+  width: 100%;
+}
 
-    .dashboard-content article p{
-        color: black;
-    }
+.dashboard-content article p {
+  color: black;
+}
 
-    /* Animação */
-    @keyframes slide-in{
-        from {transform: translateX(-300px);}
-        to {transform: translateX(0);}
-    }
-    @keyframes slide-out {
-        from {transform: translateX(0);}
-        to {transform: translateX(-300px);}
-    }
-    .slide-enter-active{
-        animation: slide-in 0.5s ease;
-    }
-    .slide-leave-active{
-        animation: slide-out 0.5s ease;
-    }
+/* Animação */
+@keyframes slide-in {
+  from {
+    transform: translateX(-300px);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-300px);
+  }
+}
+.slide-enter-active {
+  animation: slide-in 0.5s ease;
+}
+.slide-leave-active {
+  animation: slide-out 0.5s ease;
+}
 
-     /* Responsive */
-    @media (max-width:860px){
-        .container-profile button{
-            display: block;
-            padding: 8px;
-        }
-        .menu{
-            position: absolute;
-            z-index: 9999999;
-            height: 1950px;
-        }
+/* Responsive */
+@media (max-width: 860px) {
+  .container-profile button {
+    display: block;
+    padding: 8px;
+  }
+  .menu {
+    position: absolute;
+    z-index: 9999999;
+    height: 1950px;
+  }
 
-        .dashboard-content {
-            padding: 40px 40px;
-        }
-    }
+  .dashboard-content {
+    padding: 40px 40px;
+  }
+}
 
-    @media (max-width:640px){
-        .navbar{
-            display: flex;
-            align-items: center;
-            padding: 16px 20px;
-            background-color: #023F5C;
-            border-bottom: 1px solid rgb(230, 227, 227);
-        }
-        .menu{
-            width: 200px;
-        }
-        .dashboard-content {
-            padding: 30px 30px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: space-between;
-        }
-    }
+@media (max-width: 640px) {
+  .navbar {
+    display: flex;
+    align-items: center;
+    padding: 16px 20px;
+    background-color: #023f5c;
+    border-bottom: 1px solid rgb(230, 227, 227);
+  }
+  .menu {
+    width: 200px;
+  }
+  .dashboard-content {
+    padding: 30px 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+  }
+}
 
-    @media (max-width:440px) {
-        .dashboard-content {
-            padding: 20px 20px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: space-between;
-        }
-        .navbar{
-            padding: 16px 12px;
-        }
-        .navbar .container-profile{
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            align-items: center;
-            gap: 10px;
-        }
+@media (max-width: 440px) {
+  .dashboard-content {
+    padding: 20px 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .navbar {
+    padding: 16px 12px;
+  }
+  .navbar .container-profile {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+  }
 
-        .container-profile .avatar{
-            background-color: #FFFFFF;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            border-radius: 50%;
-            height: 26px;
-            width: 26px;
-        }
+  .container-profile .avatar {
+    background-color: #ffffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    height: 26px;
+    width: 26px;
+  }
 
-        .container-profile span{
-            color: #FFFFFF;
-            font-size: 1rem;
-            font-weight: 700;
-        }
+  .container-profile span {
+    color: #ffffff;
+    font-size: 1rem;
+    font-weight: 700;
+  }
 
-        .logo-home{
-            width: 26px;
-            height: 26px;
-        }
-    }
+  .logo-home {
+    width: 26px;
+    height: 26px;
+  }
+}
 </style>
