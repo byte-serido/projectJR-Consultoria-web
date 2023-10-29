c85a603667372ec984a6b7d6d3b4fdca59afa303
Adriel <adriel.fsantos@outlook.com>
2023-10-29 11:37:48 -0300

    feat: Add services data object #97

---
+++ src/data/services.js
@@ -0,0 +1,22 @@
+const services = {
+  organization: {
+    id: 1,
+    nome: 'Organização',
+    descricao:
+      'Organização é uma palavra originada do grego "organon", que significa instrumento, utensílio. Em Administração, o termo "organização" pode ter três sentidos: Associação de pessoas que combinam esforços individuais e em equipe com a finalidade de realizar propósitos colectivos.',
+  },
+  strategy: {
+    id: 2,
+    nome: 'Estratégia',
+    descricao:
+      'Estratégia é o plano de ação para alcançar metas e objetivos de longo prazo em um contexto organizacional. Ele é uma visão do futuro e uma orientação para alcançar essa visão.',
+  },
+  finance: {
+    id: 2,
+    nome: 'Finanças',
+    descricao:
+      'O dicionário Aurélio define o termo Finanças como sendo a “ciência e a profissão do manejo do dinheiro, particularmente do dinheiro do Estado”. De uma forma mais ampla, dizemos que ela trata do processo, instituições, mercados e instrumentos envolvidos na transferência de fundos entre pessoas, empresas e governos.',
+  },
+};
+
+export default services;
